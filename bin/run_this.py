644234ab985f28d1f645c3a77f3edbdccc49db31
sandhya123r <sandhyafeb1990@gmail.com>
2016-04-30 23:35:30 +0000

    finish working toy

--- bin/run_this.py
+++ bin/run_this.py
@@ -1,4 +1,8 @@
 from helloworld import hello
+print dir(hello)
+import sys
+print sys.path
 
 print "Hello World!.... Deploying"
+hello = hello.Hello()
 hello.app.run(host="0.0.0.0")
--- helloworld/hello.py
+++ helloworld/hello.py
@@ -2,11 +2,13 @@
 from flask import Flask
 from flask_restful import Resource, Api
 
-app = Flask(__name__)
-api = Api(app)
 
 class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
 
-api.add_resource(HelloWorld, '/')
+class Hello(object):
+    def __init__(self):
+        self.app = Flask(__name__)
+        self.api = Api(self.app)
+        self.api.add_resource(HelloWorld, '/')
